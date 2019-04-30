659d14f55752ef4717d601c4f4e8fd251707ea34
CaroFG <carolina.ferreira131@gmail.com>
2019-04-30 17:22:11 +0200

    form done

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,12 +1,28 @@
 class GossipsController < ApplicationController
   def index
   @gossip = Gossip.all
-  puts @gossip
   end
 
 	def show
 		@gossip_precis = Gossip.find(params[:id])
 	end  
 
+	def new
+		@gossip_new = Gossip.new
+	end
+
+	def create
+		anonymous = User.find_by(last_name: "Nymous")
+    @gossip_new = Gossip.new(title: params[:title], content: params[:content], user: anonymous)
+    @gossip_new.save
+
+    if @gossip_new.save
+    	redirect_to action:'index'
+    else
+    	render 'new' 
+    end
+
+    puts @gossip_new.user 
+	end
 	
 end
